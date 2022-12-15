e931079810a090d6b5ba8fd6d46013f8611300be
Rzalas <ruthsalvatierraistph@gmail.com>
2022-12-15 17:15:15 -0500

    Update editar_docente.php

--- editar_docente.php
+++ editar_docente.php
@@ -165,7 +165,7 @@ $res_b_docente = mysqli_fetch_array($busc_docente);
                       <div class="ln_solid"></div>
                       <div class="form-group">
                         <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
-                          <a href="Docentes.php"><button class="btn btn-primary" type="button">Cancelar</button></a>
+                          <a href="docentes.php"><button class="btn btn-primary" type="button">Cancelar</button></a>
                           
 						    <button class="btn btn-primary" type="reset">Limpiar</button>
                           <button type="submit" class="btn btn-success">Actualizar Datos</button>
